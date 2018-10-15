750f92f7f1c34a0324703ba37f8cc9b911ebb216
kaydee428 <42545862+kaydee428@users.noreply.github.com>
2018-10-15 19:48:01 +0530

    correct spacing

--- automotion.py
+++ automotion.py
@@ -7,6 +7,7 @@ database_url='https://python-1abb9.firebaseio.com'
 firebase = firebase.FirebaseApplication(database_url, None)
 
 # my sample database in firebasepython
+
 #{'ac': '0', 'cooler': '0', 'fan': '1', 'light': '1'}
 
 #get all tha database data in json
